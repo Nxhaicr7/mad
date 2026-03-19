25e7da76eb0ab6a4f1dac75b6b1ac632eb003a0e
Nguyễn Quang Huy <huynq.b22cn391@stu.ptit.edu.vn>
2026-03-19 23:08:22 +0700

    create and update, delete transaction

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -1,24 +1,47 @@
-import { colors, spacingX, spacingY } from '@/constants/theme';
-import { scale, verticalScale } from '@/utils/styling';
-import { ImageBackground, StyleSheet, View } from 'react-native';
-import * as Icons from 'phosphor-react-native';
-import Typo from './Typo';
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { useAuth } from "@/contexts/authContext";
+import useFetchData from "@/hooks/useFetchData";
+import { WalletType } from "@/types";
+import { scale, verticalScale } from "@/utils/styling";
+import { orderBy, where } from "firebase/firestore";
+import * as Icons from "phosphor-react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
+import Typo from "./Typo";
 
 const HomeCard = () => {
+  const { user } = useAuth();
+
+  const {
+    data: wallets,
+    error,
+    loading: walletLoading,
+  } = useFetchData<WalletType>("wallets", [
+    where("uid", "==", user?.uid),
+    orderBy("created", "desc"),
+  ]);
+
+  const getTotals = () => {
+    return wallets.reduce(
+      (totals: any, item: WalletType) => {
+        totals.balance = totals.balance + Number(item.amount);
+        totals.income = totals.income + Number(item.totalIncome);
+        totals.expenses = totals.expenses + Number(item.totalExpenses);
+        return totals;
+      },
+      { balance: 0, income: 0, expenses: 0 },
+    );
+  };
+
   return (
     <ImageBackground
-      source={require('../assets/images/card.png')}
+      source={require("../assets/images/card.png")}
       resizeMode="stretch"
       style={styles.bgImage}
     >
       <View style={styles.container}>
         {/* Total Balance */}
         <View style={styles.totalBalanceRow}>
-          <Typo
-            color={colors.neutral800}
-            size={17}
-            fontWeight="500"
-          >
+          <Typo color={colors.neutral800} size={17} fontWeight="500">
             Total Balance
           </Typo>
 
@@ -29,46 +52,12 @@ const HomeCard = () => {
           />
         </View>
 
-        <Typo
-          color={colors.black}
-          size={30}
-          fontWeight="bold"
-        >
-          $2343.23
+        <Typo color={colors.black} size={30} fontWeight="bold">
+          $ {walletLoading ? "----" : getTotals()?.balance?.toFixed(2)}
         </Typo>
 
         {/* Stats: Income & Expense */}
         <View style={styles.stats}>
-          {/* Expense */}
-          <View style={{ gap: verticalScale(5) }}>
-            <View style={styles.incomeExpense}>
-              <View style={styles.statsIcon}>
-                <Icons.ArrowUp
-                  size={verticalScale(15)}
-                  color={colors.black}
-                  weight="bold"
-                />
-              </View>
-
-              <Typo
-                size={16}
-                color={colors.neutral700}
-                fontWeight="500"
-              >
-                Expense
-              </Typo>
-            </View>
-
-            <View style={{ alignSelf: 'center' }}>
-              <Typo
-                size={17}
-                color={colors.rose}
-                fontWeight="600"
-              >
-                $23424
-              </Typo>
-            </View>
-          </View>
 
           {/* Income */}
           <View style={{ gap: verticalScale(5) }}>
@@ -81,22 +70,37 @@ const HomeCard = () => {
                 />
               </View>
 
-              <Typo
-                size={16}
-                color={colors.neutral700}
-                fontWeight="500"
-              >
+              <Typo size={16} color={colors.neutral700} fontWeight="500">
                 Income
               </Typo>
             </View>
 
-            <View style={{ alignSelf: 'center' }}>
-              <Typo
-                size={17}
-                color={colors.green}
-                fontWeight="600"
-              >
-                $2342
+            <View style={{ alignSelf: "center" }}>
+              <Typo size={17} color={colors.green} fontWeight="600">
+                $ {walletLoading ? "----" : getTotals()?.income?.toFixed(2)}
+              </Typo>
+            </View>
+          </View>
+
+          {/* Expense */}
+          <View style={{ gap: verticalScale(5) }}>
+            <View style={styles.incomeExpense}>
+              <View style={styles.statsIcon}>
+                <Icons.ArrowUp
+                  size={verticalScale(15)}
+                  color={colors.black}
+                  weight="bold"
+                />
+              </View>
+
+              <Typo size={16} color={colors.neutral700} fontWeight="500">
+                Expense
+              </Typo>
+            </View>
+
+            <View style={{ alignSelf: "center" }}>
+              <Typo size={17} color={colors.rose} fontWeight="600">
+                $ {walletLoading ? "----" : getTotals()?.expenses?.toFixed(2)}
               </Typo>
             </View>
           </View>
@@ -111,28 +115,28 @@ export default HomeCard;
 const styles = StyleSheet.create({
   bgImage: {
     height: scale(210),
-    width: '100%',
+    width: "100%",
   },
 
   container: {
     padding: spacingX._20,
     paddingHorizontal: scale(23),
-    height: '87%',
-    width: '100%',
-    justifyContent: 'space-between',
+    height: "87%",
+    width: "100%",
+    justifyContent: "space-between",
   },
 
   totalBalanceRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: spacingY._5,
   },
 
   stats: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
 
   statsIcon: {
@@ -142,8 +146,8 @@ const styles = StyleSheet.create({
   },
 
   incomeExpense: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: spacingY._7,
   },
 });
